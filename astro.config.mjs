12361d4801957843fd366e6dbe9428934af4b266
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-12 02:51:56 +0700

    fix: change output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'hybrid',
+    output: 'server',
     adapter: vercel(),
     integrations: [react(), tailwind()],
 });
