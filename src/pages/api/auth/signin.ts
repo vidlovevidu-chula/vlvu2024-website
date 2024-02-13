6bd57c6939f88ac120a47c36ee23f2215ef04cde
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-14 00:57:38 +0700

    fix: edit index.astro

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -1,15 +1,8 @@
 import type { APIRoute } from "astro";
-import type { Provider } from "@supabase/supabase-js";
 import { supabase } from "../../../utils/supabase";
 export const prerender = false
 
 export const GET: APIRoute = async ({ request, cookies, redirect }) => {
-  const formData = await request.formData();
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
-  const provider = formData.get("provider")?.toString();
-
-  const validProviders = ["google", "github", "discord"];
 
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
