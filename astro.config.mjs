7cde01ebf5211d8faf4819b67f40ec139ec954ee
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-14 01:03:57 +0700

    fix: change to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
+    output: "hybrid",
     site: 'https://vlvu2024-website.vercel.app/',
     adapter: vercel({
         edgeMiddleware: true,
--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../../utils/supabase";
-export const prerender = false
+export const prerender = false;
 
 export const GET: APIRoute = async ({ request, cookies, redirect }) => {
 
