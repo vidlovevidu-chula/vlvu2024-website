80a83d59120aa79b5cbb79bf4bf5528a9503cbc4
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-13 20:57:08 +0700

    fix : add astro+supabase according to astro doc

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,10 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://nratvbrsbqhnqhsyzrqo.supabase.co";
-const supabaseKey = import.meta.env.SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey as string, {
+export const supabase = createClient(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_ANON_KEY,
+  {
     auth: {
-        flowType: "pkce",
-      },
-});
+      flowType: "pkce",
+    },
+  },
+);
+
