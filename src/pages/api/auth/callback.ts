0c0ea5e5c2f8ba8e2f82ed8bcbfd7e0a464cfa51
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-14 12:34:40 +0700

    fix: change to btn

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -12,7 +12,7 @@ export const GET: APIRoute = async ({ url, cookies, redirect }) => {
   const { data, error } = await supabase.auth.exchangeCodeForSession(authCode);
 
   if (error) {
-    return new Response(error.message, { status: 500 });
+    return new Response(JSON.stringify({message: error.message}), { status: 500 });
   }
 
   const { access_token, refresh_token } = data.session;
