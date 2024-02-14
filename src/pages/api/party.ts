24f9a9234cd6082ede3c42842e52e11586c4ac2a
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-02-14 08:25:33 +0700

    fix: form

--- src/pages/api/party.ts
+++ src/pages/api/party.ts
@@ -1,28 +1,31 @@
 import type { APIRoute } from "astro";
 import { supabase } from "../../utils/supabase";
 
-export const POST: APIRoute = async ({ request }) => {
-    const datas = await request.formData();
-    console.log(datas);
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const datas = await request.formData();
+  console.log(datas);
 
-    const create_by = datas.get('create_by');
-    const party_name = datas.get('party_name');
-    const { error } = await supabase
-        .from("party")
-        .insert([
-            {
-                create_by: create_by,
-                party_name: party_name,
-                children: []
-            },
-        ])
-        .select();
-    if (error) {
-        console.log(error.message);
-        
-        return new Response(error.message);
-    }
-    else {
-        return new Response("New");
-    }
-}
+  const create_by = datas.get("create_by");
+  const party_name = datas.get("party_name");
+  const { error } = await supabase
+    .from("party")
+    .insert([
+      {
+        create_by: create_by,
+        party_name: party_name,
+        children: [],
+      },
+    ])
+    .select();
+  if (error) {
+    console.log(error.message);
+
+    return new Response(error.message);
+  } else {
+    const { data: user, error } = await supabase
+      .from("users")
+      .update({ partyname: party_name })
+      .eq("user_name", create_by);
+    return redirect("/main");
+  }
+};
